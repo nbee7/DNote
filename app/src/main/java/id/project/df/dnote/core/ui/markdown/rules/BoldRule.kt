cfe98e0a5c5ac7aedfd58ee5bff452e443e5b8e9
daffadit <daffadit0721@gmail.com>
2026-02-04 09:23:21 +0700

    feat: add interactive markdown on note Editor

---
+++ app/src/main/java/id/project/df/dnote/core/ui/markdown/rules/BoldRule.kt
@@ -0,0 +1,17 @@
+package id.project.df.dnote.core.ui.markdown.rules
+
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
+import id.project.df.dnote.core.ui.markdown.MarkdownRule
+
+class BoldRule : MarkdownRule {
+    override val pattern: Regex = Regex("(\\*\\*|__)(.*?)\\1")
+
+    override fun getStyle(matchResult: MatchResult): SpanStyle {
+        return SpanStyle(fontWeight = FontWeight.Bold)
+    }
+
+    override fun getDelimiterLengths(matchResult: MatchResult): Pair<Int, Int> {
+        return 2 to 2
+    }
+}
