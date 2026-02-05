5e2160007154db84439278af838ea31ff0dd10ac
daffadit <daffadit0721@gmail.com>
2026-02-05 10:49:47 +0700

    feat: add KeyboardAccessoryBar for shortcut markdown

--- app/src/main/java/id/project/df/dnote/core/ui/markdown/rules/BoldRule.kt
+++ app/src/main/java/id/project/df/dnote/core/ui/markdown/rules/BoldRule.kt
@@ -6,6 +6,7 @@ import id.project.df.dnote.core.ui.markdown.MarkdownRule
 
 class BoldRule : MarkdownRule {
     override val pattern: Regex = Regex("(\\*\\*|__)(.*?)\\1")
+    override val insertionSymbol: String = "**"
 
     override fun getStyle(matchResult: MatchResult): SpanStyle {
         return SpanStyle(fontWeight = FontWeight.Bold)
